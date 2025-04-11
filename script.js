543b4b92af0b54e9affc16bc56de85941dbdbf7e
Mack <awhal86@wgu.edu>
2025-04-11 16:55:54 -0600

    updated path

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 document.addEventListener('DOMContentLoaded', function () {
 
+    const isNestedPage = this.location.pathname.includes('/pages');
+    const prefix = isNestedPage ? '/D479UXDesign' : './';
     // Helper function to fetch and inject HTML content
     const loadContent = (url, containerId) => {
         return fetch(url)
@@ -16,8 +18,8 @@ document.addEventListener('DOMContentLoaded', function () {
 
     // Load the nav.html and footer.html content
     Promise.all([
-        loadContent('../components/nav.html', 'nav'),
-        loadContent('../components/footer.html', 'footer')
+        loadContent('${prefix}components/nav.html', 'nav'),
+        loadContent('${prefix}components/footer.html', 'footer')
     ]).then(([navLoaded, footerLoaded]) => {
         if (navLoaded) {
             // Once nav.html is successfully loaded, set up the hamburger icon
